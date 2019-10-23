d0ed85c3f02dcbef2ce75e610c7ad5919fb39f78
Chance McQueen <jasoncmcqueen@gmail.com>
2019-10-22 22:26:18 -0500

    updating main for error fixing (db errors)

--- mynam/urls.py
+++ mynam/urls.py
@@ -20,6 +20,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('login/', include('login.urls'))
     #path('login/', include('login.urls')),
 ]
 
